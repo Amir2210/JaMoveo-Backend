f40763127e0d91b1a09fbc572bc62a1cae0a0fdc
Amir Yankolovich <amiryankolovich2000@gmail.com>
2025-03-24 15:34:41 +0200

    Build for production

--- services/socket.service.js
+++ services/socket.service.js
@@ -15,6 +15,9 @@ export function setupSocketAPI(server) {
         socket.on('admin-set-song', song => {
             socket.broadcast.emit('admin-choose-song', song)
         })
+        socket.on('admin-search-for-new-song', song => {
+            socket.broadcast.emit('admin-pick-new-song', song)
+        })
 
         socket.on('set-user-socket', userId => {
             console.log(`Setting socket.userId = ${userId} for socket [id: ${socket.id}]`)
@@ -29,26 +32,6 @@ export function setupSocketAPI(server) {
     })
 }
 
-// function emitTo({ type, data, label }) {
-//     if (label) gIo.to('watching:' + label.toString()).emit(type, data)
-//     else gIo.emit(type, data)
-// }
-
-// async function emitToUser({ type, data, userId }) {
-//     userId = userId.toString()
-//     const socket = await _getUserSocket(userId)
-
-//     if (socket) {
-//         console.log(`Emiting event: ${type} to user: ${userId} socket [id: ${socket.id}]`)
-//         socket.emit(type, data)
-//     } else {
-//         console.log(`No active socket for user: ${userId}`)
-//         // _printSockets()
-//     }
-// }
-
-// If possible, send to all sockets BUT not the current socket 
-// Optionally, broadcast to a room / to all
 async function broadcast({ type, data, room = null, userId }) {
     userId = userId.toString()
 
@@ -75,28 +58,11 @@ async function _getUserSocket(userId) {
     return socket
 }
 async function _getAllSockets() {
-    // return all Socket instances
     const sockets = await gIo.fetchSockets()
     return sockets
 }
 
-async function _printSockets() {
-    const sockets = await _getAllSockets()
-    console.log(`Sockets: (count: ${sockets.length}):`)
-    sockets.forEach(_printSocket)
-}
-function _printSocket(socket) {
-    console.log(`Socket - socketId: ${socket.id} userId: ${socket.userId}`)
-}
-
 export const socketService = {
-    // set up the sockets service and define the API
     setupSocketAPI,
-    // emit to everyone / everyone in a specific room (label)
-    // emitTo,
-    // emit to a specific user (if currently active in system)
-    // emitToUser,
-    // Send to all sockets BUT not the current socket - if found
-    // (otherwise broadcast to a room / to all)
     broadcast,
 }
